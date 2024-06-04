bb40a7ef4ebb99645313b2f0c291c1e4d73041ca
Scott Christian <scott@codesilver.io>
2024-06-04 14:06:46 -0500

    More tweaks to run script.

--- YNAB Category Info/run_ynab_scripts.sh
+++ YNAB Category Info/run_ynab_scripts.sh
@@ -2,10 +2,12 @@
 # Make sure to change the paths to where you have this folder if you use this script.
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
     source .venv/Scripts/activate
+    pip install -r requirements.txt
     python ynab_category_spending_check.py
     python ynab_category_balances.py
 else
     source .venv/bin/activate
+    pip install -r requirements.txt
     python ynab_category_spending_check.py
     python ynab_category_balances.py
 
