8dada1b7ae6522d11c33ff5e0f951eebd25827e0
Scott Christian <scott@codesilver.io>
2024-06-04 14:02:28 -0500

    Update run script.

--- YNAB Category Info/run_ynab_scripts.sh
+++ YNAB Category Info/run_ynab_scripts.sh
@@ -3,9 +3,11 @@
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
     source .venv/Scripts/activate
     python ynab_category_spending_check.py
+    python ynab_category_balances.py
 else
     source .venv/bin/activate
     python ynab_category_spending_check.py
+    python ynab_category_balances.py
 
 fi
 deactivate
